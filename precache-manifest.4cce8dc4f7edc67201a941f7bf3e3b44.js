38fb7f5b0394b9df2b65337ecea973ec6dffc8c0
hossensyedriadh <48594108+hossensyedriadh@users.noreply.github.com>
2020-08-27 22:41:15 +0600

    Updates

---
+++ precache-manifest.4cce8dc4f7edc67201a941f7bf3e3b44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8c3f64d7b32acc3eb7108ed0a38484",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "ea0249d262e09c46752c",
+    "url": "/weather/static/css/main.e6ee2394.chunk.css"
+  },
+  {
+    "revision": "4f94aa3bb168dd90d917",
+    "url": "/weather/static/js/2.df2087d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.df2087d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0249d262e09c46752c",
+    "url": "/weather/static/js/main.f05f9108.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
